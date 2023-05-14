42624a130804517c886a03147178d40c20d068e7
Samah Alsabbagh <alsabbagh.samah@gmail.com>
2023-05-14 16:30:02 +0200

    implementing income and expenses method

--- NemBank/NemBank/src/main/java/com/jmc/nembank/nembank/Controllers/Client/DashboardController.java
+++ NemBank/NemBank/src/main/java/com/jmc/nembank/nembank/Controllers/Client/DashboardController.java
@@ -35,7 +35,7 @@ public class DashboardController implements Initializable {
         transaction_listview.setItems(Model.getInstance().getLatestTransactions());
         transaction_listview.setCellFactory(e -> new TransactionCellFactory());
         send_money_btn.setOnAction(event -> onSendMoney());
-
+        accountSummary();
     }
 
     private void bindData(){
@@ -76,6 +76,27 @@ public class DashboardController implements Initializable {
         payee_fld.setText("");
         amount_fld.setText("");
         message_fld.setText("");
+
+    }
+
+    private  void accountSummary(){
+        double income = 0;
+        double expenses = 0;
+        if(Model.getInstance().getAllTransactions().isEmpty()){
+            Model.getInstance().setAllTransactions();
+
+        }
+        for (Transaction transaction: Model.getInstance().getAllTransactions()){
+            if(transaction.senderProperty().get().equals(Model.getInstance().getClient().pAddressProperty().get())){
+                expenses = expenses + transaction.amountProperty().get();
+            }else {
+                income = income + transaction.amountProperty().get();
+            }
+
+        }
+
+        income_lbl.setText("+ $" +income);
+        expense_lbl.setText("- $" +expenses);
 
     }
 }
